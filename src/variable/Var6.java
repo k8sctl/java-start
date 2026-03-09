43342a1811e36acd91dc7ada66dae6fe2b495060
thbins <joons0218@gmail.com>
2026-03-09 23:35:18 +0900

    학습: 변수 선언, 초기화 및 미초기화 규칙 예제 작성

---
+++ src/variable/Var6.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var6 {
+
+    public static void main(String[] args) {
+        int a;
+        // System.out.println(a);
+        /*
+        변수를 초기화하지 않고 사용하면 아래 에러가 발생한다.
+        java: variable a might not have been initialized
+         */
+    }
+}
